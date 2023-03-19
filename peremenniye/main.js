ec48ad339a15bd3cadf2a2c06c10589c5c7cf3f3
mekabibifi <sarmatova.meerim.pi.9.04@gmail.com>
2023-03-19 10:14:00 +0600

    добавла домашнее задание "переменные"

---
+++ peremenniye/main.js
@@ -0,0 +1,49 @@
+//! 1 задание 
+
+// let admin = 'John'
+// let name = 'John'
+// console.log(admin)
+
+
+//! 2 задание
+
+// let str ='Привет, Мир!'
+// console.log(str)
+
+// let str1='Привет, '
+// let str2='Мир!'
+// console.log(str1 + str2)
+
+// let name = 'Meerim'
+// console.log(str1 + name)
+
+// let age = 17
+// let mne = 'Мне '
+// let god =' лет'
+// console.log(mne + age + god)
+
+
+//! 3 задание
+
+// let num = 3 
+// console.log(num)
+
+// let a = 10
+// let b = 2
+//console.log(a + b)
+// console.log(a - b)
+// console.log(b - a)
+// console.log(a * b)
+// console.log(a % b)
+// console.log(b % a)
+
+// let c = 15 
+// let d = 2
+// let result = c + d
+// console.log(result)
+
+// let a = 10
+// let b = 2
+// let c = 5
+// console.log(a + b + c)
+
